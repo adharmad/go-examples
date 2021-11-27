06195e3016a48aef5bb106c255064ae6b0c77eb7
Amol Dharmadhikari <adharmad@ionate.io>
2021-11-26 22:48:57 -0800

    Tour of go - methods - 1

---
+++ tour-of-go/methods/method-pointers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// Method pointer is used to modify the struct that is
+// being pointed. Using a regular method here would
+// leave the original struct unchanged
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
